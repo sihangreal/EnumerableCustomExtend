1e314b920b38ddcb590bdf5a3f102ca77c7e7826
陈思行 <chensh@legion-tech.net>
2018-12-12 09:06:21 +0800

    1.Enumerable的几个扩展方法

---
+++ EnumerableCustomExtend/CustomExtend.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnumerableCustomExtend
+{
+    /// <summary>
+    /// 自定义扩展
+    /// </summary>
+    public static class CustomExtend
+    {
+        /// <summary>
+        /// 自定义Distinct扩展方法
+        /// </summary>
+        /// <typeparam name="T">要去重的对象类</typeparam>
+        /// <typeparam name="C">自定义去重的字段类型</typeparam>
+        /// <param name="source">要去重的对象</param>
+        /// <param name="getfield">获取自定义去重字段的委托</param>
+        /// <returns></returns>
+        public static IEnumerable<T> MyDistinct<T, C>(this IEnumerable<T> source, Func<T, C> getfield)
+        {
+            return source.Distinct(new Compare<T, C>(getfield));
+        }
+        /// <summary>
+        /// 自定义Intersect扩展方法
+        /// </summary>
+        /// <typeparam name="T">要取交集的类</typeparam>
+        /// <typeparam name="C">自定义字段类型</typeparam>
+        /// <param name="first">第一个集合</param>
+        /// <param name="second">第二个集合</param>
+        /// <param name="getfield">获取自定义比较字段的委托</param>
+        /// <returns></returns>
+        public static IEnumerable<T> MyIntersect<T, C>(this IEnumerable<T> first, IEnumerable<T> second, Func<T, C> getfield)
+        {
+            return first.Intersect(second, new Compare<T, C>(getfield));
+        }
+
+        /// <summary>
+        /// 自定义Except扩展方法
+        /// </summary>
+        /// <typeparam name="T">要取差集的类</typeparam>
+        /// <typeparam name="C">自定义字段类型</typeparam>
+        /// <param name="first">第一个集合</param>
+        /// <param name="second">第二个集合</param>
+        /// <param name="getfield">获取自定义比较字段的委托</param>
+        /// <returns></returns>
+        public static IEnumerable<T> MyExcept<T,C>(this IEnumerable<T> first,IEnumerable<T> second,Func<T,C> getfield )
+        {
+            return first.Except(second,new Compare<T,C>(getfield));
+        }
+
+        /// <summary>
+        /// 自定义Union扩展方法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="C"></typeparam>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="getfield"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> MyUnion<T, C>(this IEnumerable<T> first, IEnumerable<T> second, Func<T, C> getfield)
+        {
+            return first.Union(second, new Compare<T, C>(getfield));
+        }
+    }
+
+    /// <summary>
+    /// 自定义比较类型
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="C"></typeparam>
+    public class Compare<T, C> : IEqualityComparer<T>
+    {
+        private Func<T, C> _getField;
+        public Compare(Func<T, C> getfield)
+        {
+            this._getField = getfield;
+        }
+        public bool Equals(T x, T y)
+        {
+            return EqualityComparer<C>.Default.Equals(_getField(x), _getField(y));
+        }
+        public int GetHashCode(T obj)
+        {
+            return EqualityComparer<C>.Default.GetHashCode(this._getField(obj));
+        }
+    }
+
+    class PersonCompare: IEqualityComparer<Person>
+    {
+        public bool Equals(Person x, Person y)
+        {
+            return x.Name == y.Name && x.Age == y.Age;
+        }
+
+        public int GetHashCode(Person obj)
+        {
+            return (obj == null) ? 0 : obj.ToString().GetHashCode();
+        }
+
+    }
+}
---
+++ EnumerableCustomExtend/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnumerableCustomExtend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> list1 = new List<Person>()
+            {
+                new Person() { Name="朱文达",Age=20},
+                new Person () { Name="徐良兴",Age=16},
+                new Person () { Name="宋应",Age=14},
+                new Person () { Name="郑捷伟",Age=14},
+            };
+
+            List<Person> list2 = new List<Person>()
+            {
+                new Person() { Name="朱文达",Age=18},
+                new Person () { Name="徐良兴",Age=16},
+                new Person () { Name="宋应",Age=14},
+                new Person () { Name="陈思行",Age=12},
+            };
+
+            //去重
+            //var list = list1.MyDistinct(v=>v.Age);
+
+            //交集
+            //var list = list1.MyIntersect(list2,v=>v.Name);
+            //var list = list1.MyIntersect(list2, v=>v.Age.GetHashCode()*v.Name.GetHashCode());
+
+            //差集
+            // var list = list1.MyExcept(list2,v=>v.Age);
+
+            //并集
+            var list = list1.MyUnion(list2,v=>v.Name);
+
+            list.ToList().ForEach(v => { Console.WriteLine(v.Name); });
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+}
